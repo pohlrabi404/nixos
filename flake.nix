37c6c6b5d31a630f65fc9006ff1e8c74698dbf61
Pohl <pohlrabi404@gmail.com>
2025-01-25 16:33:57 +0000

    (feat) add home-manager

--- flake.nix
+++ flake.nix
@@ -1,11 +1,29 @@
 {
+	description = "NixOS Main Flake";
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
-	outputs = inputs@{ self, nixpkgs, ... }: {
+	outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [ ./configuration.nix ];
+		};
+		
+		nixosConfigurations = {
+			hostname = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+					home-manager.nixosModules.home-manager
+					{
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+						home-manager.users.pohlrabi = import ./home.nix;
+					}
+				];
+			};
 		};
 	};
 }
