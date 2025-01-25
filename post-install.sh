300a760f1185a2f71a1f0213412f82e0ad5d1498
Pohl <pohlrabi404@gmail.com>
2025-01-25 19:35:15 +0900

    (chore) remove files after install

--- post-install.sh
+++ post-install.sh
@@ -7,6 +7,7 @@ sudo chown -R $USER ~/.dotfiles
 cd ~/.dotfiles
 sudo mv /etc/nixos/hardware-configuration.nix hardware-configuration.nix
 echo "./hardware-configuration.nix" > .gitignore
+rm -rf /etc/nixos
 
 sudo nix flake update #remove this after having a proper repo with flake.lock
 sudo nixos-rebuild switch --flake .
