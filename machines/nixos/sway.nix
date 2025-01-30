eb3a0faee5e90c3bc8ba7cb59f0c0d219a2e38f0
Pohl <pohlrabi404@gmail.com>
2025-01-30 09:55:36 +0000

    (feat) add dm

--- machines/nixos/sway.nix
+++ machines/nixos/sway.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # sway related
@@ -7,16 +7,19 @@
     enable = true;
   };
 
-  services.greetd.settings = {
+  services.greetd = {
     enable = true;
-    default_session = {
-      command = ''
-        ${pkgs.greetd.greetd}/bin/agreety --cmd sway \
-        --power-shutdown "sudo systemctl poweroff" \
-        --theme border=magneta;text=magneta;prompt=green;time=magneta;\
-        action=magneta;container=white;button=magneta;input=red
-      '';
-      user = "pohlrabi";
+    settings = {
+      default_session = {
+        command = lib.strings.concatStrings [
+          "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd sway"
+          " --remember --remember-user-session"
+          " --time"
+          " --asterisks *"
+          " --theme prompt=green;border=magenta;text=magenta"
+        ];
+        user = "greeter";
+      };
     };
   };
 }
